3c9937566391264d2352396d95b464867b707741
Andrew Butcher <abutcher@redhat.com>
2013-04-09 09:42:37 -0400

    Trying this for some compatability.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-ScholarshipPortal::Application.config.session_store :cookie_store, key: '_scholarship-portal_session'
+ScholarshipPortal::Application.config.session_store :cookie_store, :key => '_scholarship-portal_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
