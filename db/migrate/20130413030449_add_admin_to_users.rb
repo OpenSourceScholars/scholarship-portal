dcac83dc789bd440b81f56f9b2352d1c3500b6de
Andrew Butcher <abutcher@redhat.com>
2013-04-12 23:55:17 -0400

    Take out that mass update. Not necessary.

--- db/migrate/20130413030449_add_admin_to_users.rb
+++ db/migrate/20130413030449_add_admin_to_users.rb
@@ -1,8 +1,6 @@
 class AddAdminToUsers < ActiveRecord::Migration
   def change
     add_column :users, :admin, :boolean
-    User.all.each do |user|
-      user.update_attributes!(:admin => 'false')
     end
   end
 end
